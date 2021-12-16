6f8ca81a162b974d02293d46d90afa9386b84a36
AndrewTheArtist <a.sutt@khk.ee>
2021-12-16 18:02:50 +0200

    Presentation commit

--- front/script.js
+++ front/script.js
@@ -1,7 +1,7 @@
 const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
 const yValues = [15, 6, 14, 7, 13, 8, 12, 9, 11, 10, 10];
 
-new Chart("myChart", {
+let graph = new Chart("myChart", {
     type: "line",
     data: {
         labels: xValues,
@@ -16,7 +16,21 @@ new Chart("myChart", {
     options: {
         legend: {display: false},
         scales: {
-            yAxes: [{ticks: {min: 6, max:16}}],
+            yAxes: [{ticks: {min: 10, max:300}}],
         }
     }
 });
+
+let prices = [];
+let currentPrice = 0;
+fetch("http://localhost:3000/prices").then(res => res.json()).then(function(e){prices = e; currentPrice = prices[new Date().getHours()]});
+setTimeout(function(){
+    let array = [];
+    for(let i = 0; i<24; i++) {
+        array.push(i+":00");
+    }
+    graph.data.labels = array;
+    graph.data.datasets[0].data = prices;
+    graph.update();
+    document.querySelector("#hour-price").textContent = currentPrice+"€/MWh"
+    },150)
